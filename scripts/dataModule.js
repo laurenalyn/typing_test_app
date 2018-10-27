56e917eca9d2ead8485ef88ef81a52a96ec66beb
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-26 23:28:08 -0400

    Creates appData object

---
+++ scripts/dataModule.js
@@ -0,0 +1,12 @@
+// private
+/* ==================================================
+Indicators/ Test Control & Test Results
+================================================== */
+// var indicators = {testStarted: false, testEnded: false, totalTestTime: 0, timeLeft: 0,};
+// var results = {wpm: 0, wpmChange: 0, cpm: 0, cpmChange: 0, accuracy: 0, accuracyChange: 0,};
+var appData = {
+    indicators: {testStarted: false, testEnded: false, totalTestTime: 0, timeLeft: 0,},
+    results: {wpm: 0, wpmChange: 0, cpm: 0, cpmChange: 0, accuracy: 0, accuracyChange: 0,}
+};
+
+// public
