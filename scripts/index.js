a079c6aa34456fb930a5f8768afbdfd189af9b3a
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-26 22:15:45 -0400

    adds another function to IIFE

--- scripts/index.js
+++ scripts/index.js
@@ -6,17 +6,30 @@
 var myModule =  (function() {
     //private variables and functions
     var x = 1;
-
+        //creates a private object with 2 key value pairs
+    var car = {
+        make: 'Audi',
+        model: 'A3'
+    };
 
     //public variable and functions
 
-    // returns an object with a key valled f1 and its value is a function that returns x
+    // returns an object with a key called f1 and its value is a function that returns x
     return {
         f1: function() {
             return x;
+        },
+        f2: function() {
+            return car;
         }
     };
 })();
-// calls myModule and invokes the key's function
+// calls myModule and invokes the key's(f1) function
 console.log(myModule.f1());
+// calls myModule and involes the f2 function
+console.log(myModule.f2());
+// changes the value of the model
+myModule.f2().model = 'A6';
+// calls the updated module function
+console.log(myModule.f2());
 // ==============================*/
