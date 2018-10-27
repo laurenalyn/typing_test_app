c37d84891a2365dd3804f2cf28916941668e6aa7
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-27 14:58:31 -0400

    creates structure for eventsModule

---
+++ scripts/eventsModule.js
@@ -0,0 +1,16 @@
+var eventsModule = (function() {
+    // private
+    var addEventListeners = function() {
+        //character typing event listener
+        //click on download button event listener
+        //click on restart button event listener
+    };
+    // public
+    return {
+        //init function, initiales the test before start
+        init: function(duration, testNumber) {
+            addEventListeners();
+        }
+    };
+}());
+    
