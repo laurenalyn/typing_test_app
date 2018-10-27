81ab45712e4f2998fe8e3d0b02111b44f2069b96
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-27 13:24:18 -0400

    moved function to surround entire module

--- scripts/dataModule.js
+++ scripts/dataModule.js
@@ -1,17 +1,19 @@
+var dataModule = (function() {
+
 // private
 /* ==================================================
 Indicators/ Test Control & Test Results
 ================================================== */
 // var indicators = {testStarted: false, testEnded: false, totalTestTime: 0, timeLeft: 0,};
 // var results = {wpm: 0, wpmChange: 0, cpm: 0, cpmChange: 0, accuracy: 0, accuracyChange: 0,};
-var appData = {
-    indicators: {testStarted: false, testEnded: false, totalTestTime: 0, timeLeft: 0,},
-    results: {wpm: 0, wpmChange: 0, cpm: 0, cpmChange: 0, accuracy: 0, accuracyChange: 0,},
-    words: {currentWordIndex: 0 , testWords: [],},
-};
+    var appData = {
+        indicators: {testStarted: false, testEnded: false, totalTestTime: 0, timeLeft: 0,},
+        results: {wpm: 0, wpmChange: 0, cpm: 0, cpmChange: 0, accuracy: 0, accuracyChange: 0,},
+        words: {currentWordIndex: 0 , testWords: [],},
+    };
 
 // public
-var dataModule = (function() {
+
     return {
     //Indicator - Test Control 
     setTestTime: function(x) {}, //sets the total test time to x
