3b8f5d8717d6aa3c6e80f309cf7f319197dd1f6e
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-26 22:00:25 -0400

    Covers modular design pattern to prepare typing test app

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,22 @@
-alert("Hi!");
+// ==============================
+// Modular Design Pattern
+// ==============================
+
+// Creates an IIFE (immediately invoked function expression)
+var myModule =  (function() {
+    //private variables and functions
+    var x = 1;
+
+
+    //public variable and functions
+
+    // returns an object with a key valled f1 and its value is a function that returns x
+    return {
+        f1: function() {
+            return x;
+        }
+    };
+})();
+// calls myModule and invokes the key's function
+console.log(myModule.f1());
+// ==============================*/
