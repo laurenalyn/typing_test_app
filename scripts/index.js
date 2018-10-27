75119de826509920f7bb8bfd5ac83fcd33eefcba
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-26 22:33:15 -0400

    protects object from being overridden

--- scripts/index.js
+++ scripts/index.js
@@ -13,14 +13,21 @@ var myModule =  (function() {
     };
 
     //public variable and functions
-
+    
     // returns an object with a key called f1 and its value is a function that returns x
     return {
         f1: function() {
             return x;
         },
         f2: function() {
-            return car;
+            // when we return an object - objects are passed by reference not by value - meaning we are not returning a "read only" value.
+            // we are returning a reference to the object car making it easily mutable
+            // return car;
+            // we can fix this by instead returning a new object like this:
+            return {
+                make: car.make,
+                model: car.model,
+            }
         }
     };
 })();
@@ -29,6 +36,7 @@ console.log(myModule.f1());
 // calls myModule and involes the f2 function
 console.log(myModule.f2());
 // changes the value of the model
+    // now this line of code doesn't offect the original object "car"
 myModule.f2().model = 'A6';
 // calls the updated module function
 console.log(myModule.f2());
